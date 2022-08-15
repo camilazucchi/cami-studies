da226cf3b4f006fc6620d3f0e8c74ee76722dd23
camilazucchi <camilazucchi@outlook.com>
2022-08-15 18:45:33 -0300

    modificando estilização

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -5,7 +5,7 @@ class Button extends React.Component {
     render() {
         return (
             <button className="button">
-                Botãozinho
+                Inserir
             </button>
         )
     }
