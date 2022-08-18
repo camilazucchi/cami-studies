03f4399cc55f45c2cb0add7f42fa87e55b95eee1
camilazucchi <camilazucchi@outlook.com>
2022-08-18 11:47:48 -0300

    inserindo useEffect no timer

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,13 +1,27 @@
-import { timeToSeconds } from '../../common/utils/date'
+import { timeToSeconds } from '../../common/utils/time'
+import { useEffect, useState } from 'react'
+import tasksInterface from '../../types/tasks'
 import Button from '../button'
 import Clock from './clock'
 import style from './Timer.module.scss'
 
-export default function Timer() {
-    console.log('conversão: ', timeToSeconds('01:01:01'))
+interface Props {
+    selected: tasksInterface | undefined
+}
+
+export default function Timer({ selected }:
+    Props) {
+        const [time, setTime] = useState<number>();
+        useEffect(() => {
+            if(selected?.time) {
+                setTime(timeToSeconds(selected?.time));
+            }
+        }, [selected]);
+
     return (
         <div className={style.timer}>
             <p className={style.title}>Escolha um card e inicie o cronômetro</p>
+            Tempo: {time}
             <div className={style.clockWrapper}>
                 <Clock />
             </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -24,7 +24,7 @@ function App() {
         tasks={tasks}
         selectTask={selectTask}
       />
-      <Timer />
+      <Timer selected={selected}/>
     </div>
   );
 }
